6c46589130a70b5ecfc9f004f61dc4129622dbf5
Subbarayan Ramanathan <ramnath30@yahoo.com>
2021-01-20 22:04:32 +0000

    lab 7 Actuator changes

---
+++ src/main/java/io/pivotal/pal/tracker/MyInfoContributor.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("myprop", "Awesome!");
+    }
+}
